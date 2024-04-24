b8a1f88581bc0786b75005faf859134cf7e85ce7
Slavi Dimitrov <slavi@orbisds.com>
2024-04-24 13:07:25 +0300

    add event listener to the history button

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <Parent>
       <Core /> 
       <RightSide />
+      {isTrue && <LeftSide />}  
     </Parent>
   );
 }
--- react-weather-app/src/components/simple/Core/TopBar/HistoryButton/HistoryButton.jsx
+++ react-weather-app/src/components/simple/Core/TopBar/HistoryButton/HistoryButton.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import historyImg from "../../../../images/history.png";
 
 export default function HistoryButton() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <button id="history-button">
-      <img src={historyImg} id="h-image" onClick={console.log(1)} />
+      <img
+        src={historyImg}
+        id="h-image"
+        onClick={() => {
+          console.log(1);
+        }}
+      />
     </button>
   );
 }
